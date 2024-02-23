807862fbbf5fa0b436019a330fc8eacede4274c4
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2024-02-23 15:18:24 -0800

    new db name

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -22,7 +22,7 @@ export const connectToDatabase = async () => {
 
     cached.promise = cached.promise || 
     mongoose.connect (MONGODB_URL, { 
-        dbName: 'imaginify', bufferCommands: false
+        dbName: 'users', bufferCommands: false
     })
 
     cached.conn = await cached.promise;
